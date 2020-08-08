a267d8af7c176d911512a7a85de99085f801d8c4
David Molina <davidmolina2810@gmail.com>
2020-08-08 23:18:44 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -7,11 +7,11 @@
 #7. Return square_array
 def square_array(array)
   # your code here
-  counter = 0 
+  counter = 0
   square_array = []
   while counter < array.length do
     squared = array[counter] ** 2
     square_array.push(squared)
   end
-  squared_array    
+  squared_array
 end
